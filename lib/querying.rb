864baf3535d29e5936401368e638d19c9530099d
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 17:52:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   %w[
-    SELECT title, year
+    SELECT books.title, year
     FROM books
     LEFT JOIN series
     ON books.series_id = series.id
