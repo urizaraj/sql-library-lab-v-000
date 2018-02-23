bd0e26e776fcbf7e9f8c1819a183533e445e993c
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 17:51:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,6 +4,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
     FROM books
     LEFT JOIN series
     ON books.series_id = series.id
+    WHERE series.id = 1
+    ORDER BY year ASC
   ].join(' ')
 end
 
