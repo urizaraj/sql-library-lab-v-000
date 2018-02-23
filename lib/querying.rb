dc78c0ef0809b0bafd015d5b05aa75864095228d
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 18:09:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,7 +41,15 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  %w[].join(' ')
+  %w[
+    SELECT series.title
+    FROM characters
+    LEFT JOIN series
+    ON characters.series_id = series.id
+    GROUP BY series.title
+    ORDER BY COUNT((SELECT species WHERE species = "HUMAN")) DESC
+    LIMIT 1
+  ].join(' ')
 end
 
 def select_character_names_and_number_of_books_they_are_in
