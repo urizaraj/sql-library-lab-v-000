ed14a4a1b503d955f9290c73fe045e9d8e033ff8
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 17:56:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,6 +23,7 @@ def select_value_and_count_of_most_prolific_species
   %w[
     SELECT species, COUNT(species)
     FROM characters
+    GROUP BY species
     ORDER BY COUNT(species) DESC
     LIMIT 1
   ]
