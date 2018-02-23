d5071988804475275ac1c16eabc7088ce12a019b
Alexander Urizar <urizaraj@gmail.com>
2018-02-23 17:57:43 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,9 +34,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  %w[].join(' ')
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  %w[].join(' ')
 end
